7341ad1b7af2350d4fc12414034bd31096d216ce
Park SoYoung <1004661@gmail.com>
2023-02-12 00:25:34 +0900

    Update LoginController.java

--- Ovcos/src/com/ovcos/member/controller/LoginController.java
+++ Ovcos/src/com/ovcos/member/controller/LoginController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class LoginController
  */
-@WebServlet("/login.me")
+@WebServlet("/login.")
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
